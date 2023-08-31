95d721f56b4b43017dc2100185836504000db682
Carlos Antonio Amador Torrebiarte <calucoamador@gmail.com>
2023-08-31 15:58:14 +1000

    updated end aug

--- main.py
+++ main.py
@@ -15,12 +15,18 @@ with open("hash-rate.json", "r") as file:
 hashrates = data["hash-rate"]
 prices = data["market-price"]
 
+# Define a datetime object for the cutoff date (October 1, 2014)
+cutoff_date = datetime.datetime(2009, 1, 1)
+
 def group_by_time_period(entries):
     grouped_data = {}
     for entry in entries:
         dt = datetime.datetime.utcfromtimestamp(entry["x"] / 1000)
-        time_key = f"{dt.year}-{dt.month:02d}"
-        grouped_data.setdefault(time_key, []).append(entry["y"])
+        
+        # Only process entries that are on or after the cutoff_date
+        if dt >= cutoff_date:
+            time_key = f"{dt.year}-{dt.month:02d}"
+            grouped_data.setdefault(time_key, []).append(entry["y"])
     return grouped_data
 
 def get_monthly_closing(grouped_data):
@@ -38,28 +44,25 @@ df = pd.DataFrame({
     "Price": list(monthly_close_price.values())
 })
 
-# Regression of Hashrate on Price
-hashrate_model = sm.OLS(df["Hashrate"], sm.add_constant(df["Price"])).fit()
-print('hashrate model')
-print(hashrate_model.summary())
+min_positive_price = 0.07
+df['Price'] = df['Price'].replace(0, min_positive_price)
 
-# Regression of Price on Hashrate
-price_model = sm.OLS((df["Price"]), sm.add_constant(df["Hashrate"])).fit()
-print('price model')
-print(price_model.summary())
+print(df.head())
 
-df["Log_Price"] = np.log(df["Price"] + 0.0001)
+
+df["Log_Price"] = np.log(df["Price"])
+df["Log_Hashrate"] = np.log(df["Hashrate"])
 # Regression of Log of Hashrate on Log of Price
-hashrate_model = sm.OLS(np.log(df["Hashrate"]), sm.add_constant((df["Log_Price"]))).fit()
+hashrate_model = sm.OLS(df["Log_Hashrate"], sm.add_constant((df["Log_Price"]))).fit()
 print('Log hashrate model')
 print(hashrate_model.summary())
 
 # Regression of Log of Price on Log of Hashrate
-price_model = sm.OLS(df["Log_Price"], sm.add_constant(df["Hashrate"])).fit()
+price_model = sm.OLS(df["Log_Price"], sm.add_constant(df["Log_Hashrate"])).fit()
 print('Log price model')
 print(price_model.summary())
 
-result = adfuller(np.log(df["Hashrate"]))
+result = adfuller(df["Log_Hashrate"])
 print('Augmented Dickey Fuller for Log_Hashrate')
 print('ADF Statistic:', result[0])
 print('p-value:', result[1])
@@ -70,21 +73,22 @@ if result[0] < result[4]["5%"]:
 else:
     print("The Log Hashrate time series is not stationary.")
     df['Hashrate_diff'] = df['Hashrate'].diff()
+    df['Hashrate_diff_2'] = df['Hashrate_diff'].diff()
     df.dropna(inplace=True)  # Drop NaN values
 
     print("\nTesting first difference:")
-    result_diff = adfuller(df['Hashrate_diff'])
+    result_diff = adfuller(df['Hashrate_diff_2'])
     print('ADF Statistic for differenced series:', result_diff[0])
     print('p-value:', result_diff[1])
     print('Critical Values:', result_diff[4])
 
     if result_diff[0] < result_diff[4]["5%"]:
-        print("The Log Hashrate first difference time series is stationary.")
+        print("The Log Hashrate second difference time series is stationary.")
     else:
         print("The Log Hashrate first difference time series is still not stationary.")
 
-df["Log_Price"] = np.log(df["Price"] + 0.001)
 result = adfuller((df["Log_Price"]))
+print('')
 print('Augmented Dickey Fuller for Log_Price')
 print('ADF Statistic:', result[0])
 print('p-value:', result[1])
@@ -106,28 +110,21 @@ else:
 
     if result_diff[0] < result_diff[4]["5%"]:
         print("The Log Price first difference time series is stationary.")
+        print('')
     else:
         print("The Log Price first difference time series is still not stationary.")
+        print('')
 
-# Autoregressive Model (AR) for Hashrate Prediction
-ar_model = AutoReg(df["Hashrate"], lags=2).fit()
-print('hashrate AR model')
-print(ar_model.summary())
-
-ar_model_log = AutoReg(np.log(df["Hashrate"]), lags=2).fit()
+ar_model_log = AutoReg(df["Log_Hashrate"], lags=2).fit()
 print('log of hashrate AR model')
 print(ar_model_log.summary())
 
-month_predict = 60
-future_hashrate_ar = ar_model.predict(start=len(df), end=len(df) + month_predict)
-
-ar_model_log = AutoReg(np.log(df["Hashrate"]), lags=1).fit()
+month_predict = 65
 future_hashratelog_ar = ar_model_log.predict(start=len(df), end=len(df) + month_predict)
 
-df["Log_Price"] = np.log(df["Price"] + 0.001)
 # 2. Develop SARIMAX model using log-transformed values
 sarimax_log_price_model = sm.tsa.SARIMAX((df["Log_Price"]),
-                                         exog=np.log(df["Hashrate"]),
+                                         exog=df["Log_Hashrate"],
                                          order=(1, 1, 1),
                                          seasonal_order=(1, 1, 0, 48),  # 48 months for the 4-year halving cycle
                                          enforce_stationarity=False,
@@ -162,20 +159,21 @@ print("Testing if Price Granger causes Hashrate:")
 print("Testing if Hashrate Granger causes Price:")
 #gc_hashrate_on_price = grangercausalitytests(df[['Price', 'Hashrate']], maxlag=12, verbose=True)
 
-# Assuming you've computed Log_Hashrate and stored in df
-df['Log_Hashrate'] = np.log(df['Hashrate'])
-
 # Plot ACF
 sm.graphics.tsa.plot_acf(df['Log_Hashrate'], lags=24)
 plt.title("Autocorrelation Function for Log_Hashrate")
 plt.show()
 
 # Plot ACF
-df["Log_Price"] = np.log(df["Price"] + 0.001)
+df["Log_Price"] = (df["Log_Price"])
 sm.graphics.tsa.plot_acf(df['Log_Price'], lags=24)
 plt.title("Autocorrelation Function for Log_Price")
 plt.show()
 
 sm.graphics.tsa.plot_pacf(df['Log_Hashrate'], lags=24)
 plt.title("Partial Autocorrelation Function for Log_Hashrate")
+plt.show()
+
+sm.graphics.tsa.plot_pacf(df['Log_Price'], lags=24)
+plt.title("Partial Autocorrelation Function for Log_Price")
 plt.show()
